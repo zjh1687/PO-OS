2c3cc1e545c986e4d394bc06d5190089c1d1359a
sckim <sc.kim@interxlab.com>
2026-06-10 15:18:19 +0900

    Update Next generated types notice

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,4 +1,5 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 
-// This file is automatically maintained by Next.js.
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/app/building-your-application/configuring/typescript for more information.
